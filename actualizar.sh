f746ea5022b0632d9778ebc61125b6c7cd8676f1
jesuscantillo1018-creator <jesuscantillo1018@gmail.com>
2026-02-12 23:42:38 -0600

    Actualización automática: 12-02-2026 23:42

--- actualizar.sh
+++ actualizar.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 echo "🚀 Iniciando actualización de Jeycamontv..."
 
-# 1. Ejecutar el script de Python
+# 1. Ejecutar el script interactivo (Aquí es donde te preguntará)
 python pro_list.py
 
-# 2. Preparar los archivos para Git
+# 2. Preparar los cambios para GitHub
 git add .
 
-# 3. Hacer el commit con la fecha actual
-fecha=$(date +'%d-%m-%Y %H:%M')
+# 3. Crear el mensaje con la fecha actual
+fecha=$(date +"%d-%m-%Y %H:%M")
 git commit -m "Actualización automática: $fecha"
 
-# 4. Subir a GitHub
 echo "Wait... Subiendo cambios a la nube ☁️"
+
+# 4. Empujar los cambios (usando rebase para evitar errores de tráfico)
+git pull origin maestro --rebase
 git push origin maestro
 
 echo "✅ ¡Todo listo! Tu TV ya tiene los canales actualizados."
